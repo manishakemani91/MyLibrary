05b0ceb88c0d8123472546110810ac69776e3d6e
manishakemani91 <manishakemani91@gmail.com>
2016-10-24 17:00:52 +0530

    Library project setup

---
+++ Library/Library/Library/Controllers/BookController.cs
@@ -0,0 +1,95 @@
+﻿using MyLibraryService;
+using MyLibraryService.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class BookController : Controller
+    {
+        IBookStore _bookStore;
+
+        public BookController(IBookStore bookstore)
+        {
+            _bookStore = bookstore;
+        }
+
+        /// <summary>
+        /// shows list of books
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            ViewBag.categorydd1 = new SelectList(_bookStore.GetAllCategories(), "Cat_Id", "Cat_Name", "select");
+            var bookList = _bookStore.GetAllBooks();
+            return View(bookList);
+        }
+
+        /// <summary>
+        /// Add a new book based on category
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create()
+        {
+            ViewBag.categorydd1 = new SelectList(_bookStore.GetAllCategories(), "Cat_Id", "Cat_Name", "select");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(BookInfo book)
+        {
+            _bookStore.AddBook(book);
+            return RedirectToAction("Index", "Book");
+        }
+
+        /// <summary>
+        /// Get List of books based on category selected
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Details(int? id)
+        {
+            ViewBag.categorydd1 = new SelectList(_bookStore.GetAllCategories(), "Cat_Id", "Cat_Name", "select");
+            List<BookInfo> bookList = new List<BookInfo>();
+            bookList = _bookStore.GetBooksByCategoryId(Convert.ToInt32(id));
+            return View(bookList);
+        }
+
+        /// <summary>
+        /// update book details
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Edit(int id)
+        {
+            ViewBag.categorydd1 = new SelectList(_bookStore.GetAllCategories(), "Cat_Id", "Cat_Name", "select");
+            var bookDetails = _bookStore.GetBookById(id);
+            return View(bookDetails);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(BookInfo book)
+        {
+            
+             _bookStore.UpdateBook(book);
+            return RedirectToAction("Index", "Book");
+        }
+
+        /// <summary>
+        /// delete a book from list of books
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Delete(int id)
+        {
+            
+            var bookDetails = _bookStore.GetBookById(id);
+            _bookStore.DeleteBook(bookDetails);
+            return RedirectToAction("Index", "Book");
+        }
+
+    }
+}
---
+++ Library/Library/MyLibraryService/Implentation/BookStore.cs
@@ -0,0 +1,92 @@
+﻿using MyLibraryService.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+
+namespace MyLibraryService.Implentation
+{
+    /// <summary>
+    /// class with implemenation of bookStore interface to perform CRUD operation on book library
+    /// </summary>
+    public class BookStore : IBookStore
+    {
+        BookReposEntities entities = null;
+
+        public BookStore(BookReposEntities entities)
+        {
+            this.entities = entities;
+        }
+
+        /// <summary>
+        /// get list of books
+        /// </summary>
+        /// <returns>list of books</returns>
+        public List<BookInfo> GetAllBooks()
+        {
+            return entities.BookInfoes.ToList();
+        }
+
+        /// <summary>
+        /// get list of categories
+        /// </summary>
+        /// <returns>list of categories</returns>
+        public List<Category> GetAllCategories()
+        {
+            return entities.Categories.ToList();
+        }
+
+        /// <summary>
+        /// get book details based on book id
+        /// </summary>
+        /// <param name="id">book id</param>
+        /// <returns>book info</returns>
+        public BookInfo GetBookById(int id)
+        {
+            return entities.BookInfoes.SingleOrDefault(book => book.Id == id);
+        }
+
+        /// <summary>
+        /// get list of books based on category selected
+        /// </summary>
+        /// <param name="id">category id</param>
+        /// <returns>book list</returns>
+        public List<BookInfo> GetBooksByCategoryId(int id)
+        {
+            return entities.BookInfoes.Where(book => book.Category_Id == id).ToList();
+        }
+
+        /// <summary>
+        /// add a new book
+        /// </summary>
+        /// <param name="book">book info</param>
+        public void AddBook(BookInfo book)
+        {
+            entities.BookInfoes.Add(book);
+            entities.SaveChanges();
+        }
+
+        /// <summary>
+        /// update book info
+        /// </summary>
+        /// <param name="book">book info</param>
+        public void UpdateBook(BookInfo book)
+        {
+            entities.BookInfoes.Attach(book);
+            entities.Entry(book).State = EntityState.Modified;
+            entities.SaveChanges();
+        }
+
+        /// <summary>
+        /// remove a book
+        /// </summary>
+        /// <param name="book">book info</param>
+        public void DeleteBook(BookInfo book)
+        {
+            entities.BookInfoes.Remove(book);
+            entities.SaveChanges();
+        }
+    }
+}
